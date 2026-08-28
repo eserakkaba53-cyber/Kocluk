801054376ad0ed882bffd28241ee4ac0c93c64eb
eserakkaba53-cyber <eserakkaba53@gmail.com>
2026-08-29 00:00:14 +0300

    Add gc property for tracking in sunucu.js

--- sunucu.js
+++ sunucu.js
@@ -68,3 +68,5 @@ window.OGRENCI_VIDEOLAR = [
 window.SAYILAR = { ogretmen:'+40', ogrenci:'+70' };
 
 window.BIYOSER_WP = '905325874492';   // ülke kodu, başında + ve boşluk yok
+
+gc: "https://biyoser.goatcounter.com/count"
